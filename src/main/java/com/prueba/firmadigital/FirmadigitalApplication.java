2fc908b4ef3c77ff9bce4d34da5249cc7c9693d7
SilverioMG <atopecode.gmail.com>
2025-04-12 14:36:16 +0200

    Configuración B.D. H2 en memoria.

---
+++ src/main/java/com/prueba/firmadigital/FirmadigitalApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.firmadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmadigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmadigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/firmadigital/FirmadigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.firmadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmadigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
